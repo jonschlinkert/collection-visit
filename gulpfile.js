c4c1a148f31bfbd87164a59d3ebc3bdb0c5e1a66
Brian Woodward <brian.woodward@gmail.com>
2016-08-05 16:33:33 -0400

    lint

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,26 @@ var gulp = require('gulp');
 
 var lint = ['index.js', 'utils.js'];
 
-gulp.task('lint', function () {
-  return gulp.src(lint)
+gulp.task('lint', function() {
+  return gulp.src(lint.concat(['gulpfile.js', 'test.js']))
     .pipe(eslint())
+    .pipe(eslint.format());
 });
 
-gulp.task('coverage', function () {
+gulp.task('coverage', function() {
   return gulp.src(lint)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['coverage'], function () {
+gulp.task('test', ['coverage'], function() {
   return gulp.src('test.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports())
     .pipe(istanbul.writeReports({
       reporters: [ 'text' ],
       reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
+    }));
 });
 
 gulp.task('default', ['lint', 'test']);
--- test.js
+++ test.js
@@ -5,10 +5,10 @@ require('should');
 var assert = require('assert');
 var visit = require('./');
 
-describe('arrays', function () {
+describe('arrays', function() {
   var ctx = {
     data: {},
-    set: function (key, value) {
+    set: function(key, value) {
       if (Array.isArray(key)) {
         visit(ctx, 'set', key);
       } else if (typeof key === 'object') {
@@ -19,12 +19,12 @@ describe('arrays', function () {
     }
   };
 
-  describe('visit', function () {
-    it('should call visit on every value in the given object:', function () {
+  describe('visit', function() {
+    it('should call visit on every value in the given object:', function() {
       ctx.set('a', 'a');
       ctx.set([{b: 'b'}, {c: 'c'}]);
       ctx.set({d: {e: 'f'}});
-      ctx.data.should.eql( {
+      ctx.data.should.eql({
         a: 'a',
         b: 'b',
         c: 'c',
@@ -32,8 +32,8 @@ describe('arrays', function () {
       });
     });
 
-    it('should work when the value is a string:', function () {
-      ctx.setName = function (str) {
+    it('should work when the value is a string:', function() {
+      ctx.setName = function(str) {
         this.name = str;
       };
 
@@ -44,7 +44,7 @@ describe('arrays', function () {
   });
 });
 
-describe('objects', function () {
+describe('objects', function() {
   var obj = {
     a: 'a',
     b: 'b',
@@ -54,7 +54,7 @@ describe('objects', function () {
 
   var ctx = {
     data: {},
-    set: function (key, value) {
+    set: function(key, value) {
       if (typeof key === 'object') {
         visit(ctx, 'set', key);
       } else {
@@ -63,8 +63,8 @@ describe('objects', function () {
     }
   };
 
-  describe('visit', function () {
-    it('should call visit on every value in the given object:', function () {
+  describe('visit', function() {
+    it('should call visit on every value in the given object:', function() {
       ctx.set('a', 'a');
       ctx.set('b', 'b');
       ctx.set('c', 'c');
@@ -73,17 +73,17 @@ describe('objects', function () {
     });
   });
 
-  describe('errors', function () {
-    it('should throw an error when invalid args are passed:', function () {
-      (function () {
+  describe('errors', function() {
+    it('should throw an error when invalid args are passed:', function() {
+      (function() {
         visit();
       }).should.throw('object-visit expects `thisArg` to be an object.');
 
-      (function () {
+      (function() {
         visit('foo', 'bar');
       }).should.throw('object-visit expects `thisArg` to be an object.');
 
-      (function () {
+      (function() {
         visit({}, {}, {});
       }).should.throw('object-visit expects `method` name to be a string');
     });
